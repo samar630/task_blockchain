acee740b10808618b9a97b428a99af9827b4b8a8
SamarQaddour <samarqaddour051@gmail.com>
2024-03-11 15:49:13 -0800

    finish task

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import  Welcome  from "./components/Welcome";
+
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Welcome />
+    </div>
+ 
+
+    
+  </div>
+);
+
+export default App;
